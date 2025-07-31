cbc2687f53c0fbb2d67b469a542c62297a5b4d58
Swapnil Bokade <sbokade181@gmail.com>
2025-07-31 14:04:00 +0530

    Initial commit of spring security basic project

---
+++ src/main/java/com/example/SpringSecurityBasic/HomeController.java
@@ -0,0 +1,23 @@
+package com.example.SpringSecurityBasic;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    /**
+     * To Access end points add console generated pass word to password field and username is user.
+     * Acess using postman in authorization tab select basic auth , username is user and password generated on console
+     * @return
+     */
+    @GetMapping("/user")
+    public String userAPI() {
+        return "Hello USER!";
+    }
+
+    @GetMapping("/admin")
+    public String adminAPI() {
+        return "Hello ADMIN!";
+    }
+}
