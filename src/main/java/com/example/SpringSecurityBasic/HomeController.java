68f8e19cf27141bc0714d07b2d307e79002c371c
Swapnil Bokade <sbokade181@gmail.com>
2025-08-01 16:05:25 +0530

    added comment of usage in controller

--- src/main/java/com/example/SpringSecurityBasic/HomeController.java
+++ src/main/java/com/example/SpringSecurityBasic/HomeController.java
@@ -9,6 +9,12 @@ public class HomeController {
     /**
      * To Access end points add console generated pass word to password field and username is user.
      * Acess using postman in authorization tab select basic auth , username is user and password generated on console
+     *
+     * Access By Custom Credential in application.properties file
+     * By Postman in authorization tab select Basic Auth pass username and password from application.properties file
+     *
+     * Access by In Memory Credential and Role Based
+     *  By Postman in authorization tab select Basic Auth pass username and password from SpringConfig.java as per role and end point to access
      * @return
      */
     @GetMapping("/user")
